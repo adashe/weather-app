b1da0cc7304fe958bee18a183c6fcabd6511832b
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-08-30 16:37:48 -0700

    Completed app

---
+++ scripts/app.js
@@ -0,0 +1,59 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Imperial.Value}</span>
+        <span>&degF</span>
+    </div>
+    `;
+
+    // update night/day & icon images
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    // remove d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    };
+
+};
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with the new city
+    forecast.updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+
+    // set local storage
+    localStorage.setItem('city', city);
+
+});
+
+if(localStorage.getItem('city')){
+    forecast.updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+};
